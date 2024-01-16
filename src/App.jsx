8444a5b43035424629d309749342a8b22da1d0be
Evdokia <vladalt1231@gmail.com>
2024-01-16 16:31:35 +0900

    quick fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "@/pages/Main";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Main/>}></Route>
-                <Route path="/delivery" element={<Main/>}></Route>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter basename="/Delivery">
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/delivery" element={<Main />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
